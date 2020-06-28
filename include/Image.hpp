180223d247a5144774527de51295aae906d716a2
Gabor Bencze <b.gabor98@gmail.com>
2020-06-28 19:21:13 +0200

    Create Image class

---
+++ include/Image.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Color.hpp>
+#include <vector>
+
+namespace RayMan {
+  class Image {
+  public:
+    Image(int height, int width);
+
+    Color Get(int row, int col) const;
+    void Set(int row, int col, const Color& c);
+
+    int GetHeight() const;
+    int GetWidth() const;
+
+  private:
+    const int height;
+    const int width;
+
+    std::vector<std::vector<Color>> pixels;
+  };
+}  // namespace RayMan
---
+++ source/Image.cpp
@@ -0,0 +1,16 @@
+#include <Image.hpp>
+
+namespace RayMan {
+
+  Image::Image(int height, int width)
+      : height(height), width(width), pixels(height, std::vector<Color>(width, Color(0, 0, 0))) {}
+
+  Color Image::Get(int row, int col) const { return pixels[row][col]; }
+
+  void Image::Set(int row, int col, const Color& c) { pixels[row][col] = c; }
+
+  int Image::GetHeight() const { return height; }
+
+  int Image::GetWidth() const { return width; }
+
+}  // namespace RayMan
