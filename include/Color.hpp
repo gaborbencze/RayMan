b213088ca167009fb99ed996ead817e572efa981
Gabor Bencze <b.gabor98@gmail.com>
2020-06-28 16:28:54 +0200

    create Color class

---
+++ include/Color.hpp
@@ -0,0 +1,15 @@
+namespace RayMan {
+  class Color {
+  public:
+    constexpr Color(double r, double g, double b) noexcept : _r(r), _g(g), _b(b) {}
+
+    constexpr double r() const noexcept { return _r; }
+    constexpr double g() const noexcept { return _g; }
+    constexpr double b() const noexcept { return _b; }
+
+  private:
+    double _r;
+    double _g;
+    double _b;
+  };
+}  // namespace RayMan
