db209bbb2b2bcaaf14a44426c344e7c7d9ac68cf
Gabor Bencze <b.gabor98@gmail.com>
2020-06-28 16:06:45 +0200

    Add Point3 class

---
+++ include/Point3.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Vector3.hpp>
+
+namespace RayMan {
+  class Point3 {
+  public:
+    constexpr Point3(double x, double y, double z) noexcept : vec(x, y, z) {}
+
+    constexpr double x() const noexcept { return vec.x(); }
+    constexpr double y() const noexcept { return vec.y(); }
+    constexpr double z() const noexcept { return vec.z(); }
+
+    constexpr Point3& operator+=(const Vector3& offset) noexcept {
+      vec += offset;
+      return *this;
+    }
+
+  private:
+    Vector3 vec;
+  };
+
+  constexpr Point3 operator+(const Point3& p, const Vector3& v) noexcept {
+    Point3 result = p;
+    result += v;
+    return result;
+  }
+
+}  // namespace RayMan
