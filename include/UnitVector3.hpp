ca96c76d71203fa017f3b5b743ba58cb24bdfa6e
Gabor Bencze <b.gabor98@gmail.com>
2020-06-29 20:19:47 +0200

    UnitVector3 improvements:
    - add constructor with 3 double params (mostly used in tests)
    - add operator-
    - create overloads for Dot and Cross taking UnitVector3 instances

--- include/UnitVector3.hpp
+++ include/UnitVector3.hpp
@@ -6,8 +6,7 @@ namespace RayMan {
   class UnitVector3 {
   public:
     explicit UnitVector3(const Vector3& v);
-
-    Vector3 operator*(const double t) const noexcept;
+    UnitVector3(double x, double y, double z);
 
     double x() const noexcept { return vec.x(); }
     double y() const noexcept { return vec.y(); }
@@ -15,9 +14,15 @@ namespace RayMan {
 
     Vector3 ToVector3() const noexcept { return vec; }
 
+    Vector3 operator*(const double t) const noexcept;
+    UnitVector3 operator-() const noexcept;
+
   private:
     Vector3 vec;
   };
+
+  double Dot(const UnitVector3& u, const UnitVector3& v);
+  Vector3 Cross(const UnitVector3& u, const UnitVector3& v);
 
   std::ostream& operator<<(std::ostream& os, const UnitVector3& v);
 }  // namespace RayMan
--- source/UnitVector3.cpp
+++ source/UnitVector3.cpp
@@ -5,7 +5,19 @@ namespace RayMan {
 
   UnitVector3::UnitVector3(const Vector3& v) : vec(v / v.length()) {}
 
+  UnitVector3::UnitVector3(double x, double y, double z) : UnitVector3{Vector3(x, y, z)} {}
+
   Vector3 UnitVector3::operator*(const double t) const noexcept { return vec * t; }
+
+  UnitVector3 UnitVector3::operator-() const noexcept { return UnitVector3(-vec); }
+
+  double Dot(const UnitVector3& u, const UnitVector3& v) {
+    return Dot(u.ToVector3(), v.ToVector3());
+  }
+
+  Vector3 Cross(const UnitVector3& u, const UnitVector3& v) {
+    return Cross(u.ToVector3(), v.ToVector3());
+  }
 
   std::ostream& operator<<(std::ostream& os, const UnitVector3& v) {
     os << v.ToVector3();
