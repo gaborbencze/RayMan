afaa1a341d16295c2c045286e3394680e6b9692b
Gabor Bencze <b.gabor98@gmail.com>
2020-07-08 20:43:43 +0200

    multisampling

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -7,8 +7,13 @@
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <random>
 
 static constexpr int MaxColorValue = 255;
+
+static constexpr int ImageWidth = 960;
+static constexpr int ImageHeight = 540;
+static constexpr int SamplesPerPixel = 50;
 
 static void PrintColor(std::ostream& os, const RayMan::Color& c) {
   const int r = c.r() * MaxColorValue;
@@ -51,7 +56,34 @@ static RayMan::Scene GetWorld() {
   return world;
 }
 
-static RayMan::Image RenderImage(int width, int height) {
+static RayMan::Color GetPixelColor(const RayMan::Image& image, const RayMan::Scene& world,
+                                   const RayMan::Camera& camera, int samples, int row, int col) {
+  // TODO: We should be able to call this from multiple threads
+  // Making these static prevents this
+  static std::uniform_real_distribution<double> distribution(0, 1);
+  static std::mt19937 generator;
+
+  double sum_r = 0;
+  double sum_g = 0;
+  double sum_b = 0;
+
+  for (int i = 0; i < samples; ++i) {
+    const auto u = static_cast<double>(col + distribution(generator)) / image.GetWidth();
+    const auto v = 1 - (static_cast<double>(row + distribution(generator)) / image.GetHeight());
+    const auto ray = camera.GetRay(u, v);
+    const auto color = RayColor(ray, world);
+
+    sum_r += color.r();
+    sum_g += color.g();
+    sum_b += color.b();
+  }
+
+  const auto scaleComponent = [samples](const double value) { return value / samples; };
+
+  return RayMan::Color(scaleComponent(sum_r), scaleComponent(sum_g), scaleComponent(sum_b));
+}
+
+static RayMan::Image RenderImage(int width, int height, int samples) {
   RayMan::Image img(height, width);
   const RayMan::Camera camera;
 
@@ -59,13 +91,10 @@ static RayMan::Image RenderImage(int width, int height) {
 
   for (int row = 0; row < img.GetHeight(); ++row) {
     for (int col = 0; col < img.GetWidth(); ++col) {
-      const auto u = static_cast<double>(col) / (width - 1);
-      const auto v = 1 - (static_cast<double>(row) / (height - 1));
-      const auto ray = camera.GetRay(u, v);
-      img.Set(row, col, RayColor(ray, world));
+      img.Set(row, col, GetPixelColor(img, world, camera, samples, row, col));
     }
   }
   return img;
 }
 
-int main() { PrintPPMImage(std::cout, RenderImage(1920, 1080)); }
+int main() { PrintPPMImage(std::cout, RenderImage(ImageWidth, ImageHeight, SamplesPerPixel)); }
